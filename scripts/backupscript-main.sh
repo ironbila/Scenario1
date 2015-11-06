a222848f695a7dc87009ca8f175d6ac3ab1c0183
ironbila <octav.rusuleanu@yahoo.com>
2015-11-06 14:20:30 +0000

    reversed backup

--- scripts/backupscript-main.sh
+++ scripts/backupscript-main.sh
@@ -5,4 +5,4 @@ backupfile="dbbackup-$(date "+%Y%m%d%H%M").bak"
 
 echo "storing backup database in" $backupfile
 
-ssh localuser@studvm16-p "cd $directory && sqlite3 db.sqlite3 .dump > $backupfile && scp $backupfile localuser@studvm53-p:Documents/backup && rm $backupfile"
+ssh localuser@studvm53-p "cd $directory && sqlite3 db.sqlite3 .dump > $backupfile && scp $backupfile localuser@studvm16-p:Documents/backup && rm $backupfile"
--- scripts/restorescript-main.sh
+++ scripts/restorescript-main.sh
@@ -7,4 +7,4 @@ backupfile=`ls -r dbbackup*.bak | sort -n -t _ -k 2 | tail -1`
 
 echo "restoring database"
 scp $backupfile localuser@studvm16-p:$directory
-ssh localuser@studvm16-p "cd $directory && mv db.sqlite3 db.sqlite3.old && sqlite3 db.sqlite3 < $backupfile && rm $backupfile"
+ssh localuser@studvm53-p "cd $directory && mv db.sqlite3 db.sqlite3.old && sqlite3 db.sqlite3 < $backupfile && rm $backupfile"
