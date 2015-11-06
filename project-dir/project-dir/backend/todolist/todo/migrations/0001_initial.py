efd71678ef06e4373389c165a6c694034dbff22c
YeeChong94 <yeechong1994@gmail.com>
2015-11-06 02:34:43 +0000

    Django Done
    Done with Django modelling and SQLite. Set up the relationships between
    files, just need to add in templates and static files.

---
+++ project-dir/project-dir/backend/todolist/todo/migrations/0001_initial.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('description', models.CharField(max_length=200)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Permissions',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('list', models.OneToOneField(to='todo.List')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Todo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('description', models.CharField(max_length=200)),
+                ('done', models.BooleanField(default=False)),
+                ('list', models.ForeignKey(to='todo.List')),
+            ],
+        ),
+    ]
---
+++ project-dir/project-dir/backend/todolist/todo/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
+
+
+urlpatterns = [
+     url(r'^$', views.index, name='index'),
+     url(r'^viewTodo$', views.view_todo),
+     url(r'^addTodo/(?P<id>[1-9]+)/$', views.create_todo),
+	 url(r'^deleteTodo/(?P<id>[1-9]+)/$', views.delete_todo),
+	 url(r'^completeTodo/(?P<id>[1-9]+)/$', views.complete_todo),
+     url(r'^viewList$', views.view_lists),
+     url(r'^addList$', views.create_list),
+     url(r'^deleteList/(?P<id>[1-9]+)/$', views.delete_list),
+     url(r'^newList$', views.view_list_form),
+     url(r'^editList/(?P<id>[1-9]+)/$', views.edit_list_form),
+     url(r'^editTodo/(?P<id>[1-9]+)/$',views.edit_todo_form),
+     url(r'^changeList/(?P<id>[1-9]+)/$', views.edit_list),
+     url(r'^changeTodo/(?P<id>[1-9]+)/$', views.edit_todo),
+
+    #User accounts stuff
+
+
+]
