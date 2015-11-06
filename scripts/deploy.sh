37fd3a746e57abc91fb2c88fa301495d9d92c3e1
ironbila <octav.rusuleanu@yahoo.com>
2015-11-06 10:18:49 +0000

    changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,17 +10,17 @@ sudo systemctl stop httpd
 
 # start deploying
 # copy web app content in the html folder
-sudo cp django-rep/project-dir/. /var/www/html/ -R
+sudo cp Scenario1/project-dir/. /var/www/html/ -R
 
 # copy .conf file to apache 
-sudo cp django-rep/utils/finalconfig.conf /etc/httpd/conf.d/ -R
+sudo cp Scenario1/utils/finalconfig.conf /etc/httpd/conf.d/ -R
 
 # solve permission issues
 sudo usermod -a -G localuser apache
 sudo chmod 710 /home/localuser
-sudo chmod 664 /var/www/html/project-dir/backend/db.sqlite3
-sudo chown :apache /var/www/html/project-dir/backend/db.sqlite3
-sudo chown :apache /var/www/html/project-dir/backend
+sudo chmod 664 /var/www/html/project-dir/backend/todolist/db.sqlite3
+sudo chown apache /var/www/html/project-dir/backend/todolist/db.sqlite3
+sudo chown apache /var/www/html/project-dir/backend/todolist
 
 # start server
 sudo systemctl start httpd
