1b72821d1f2377b13026b6dd1a0cb55579632839
ironbila <octav.rusuleanu@yahoo.com>
2015-11-06 12:53:59 +0000

    changed fabfile

--- fabric-scripts/fabfile.py
+++ fabric-scripts/fabfile.py
@@ -1,9 +1,34 @@
 from fabric.api import *
 
-#env.users='orusulea'
+#env.users='localuser'
 
-env.hosts=['orusulea@newgate.cs.ucl.ac.uk']
+env.hosts=['localuser@studvm16-p.cs.ucl.ac.uk']
 
 
-def testing():
-	run('touch ahahahah_it_works.txt')
+def deploy():
+	run("git clone https://github.com/ironbila/Scenario1")
+    sudo("systemctl stop httpd")
+    sudo("cp Scenario1/project-dir/. /var/www/html/ -R")
+    sudo("cp Scenario1/utils/finalconfig.conf /etc/httpd/conf.d/ -R")
+    sudo("usermod -a -G localuser apache")
+    sudo("chmod 710 /home/localuser")
+    sudo("chmod 664 /var/www/html/project-dir/backend/todolist/db.sqlite3")
+    sudo("chown apache /var/www/html/project-dir/backend/todolist/db.sqlite3")
+    sudo("chown apache /var/www/html/project-dir/backend/todolist")
+    sudo("systemctl start httpd")
+    sudo("sudo rm -rf Scenario1")
+
+def environment_set_up():
+    sudo("yum install epel-release")
+    sudo("yum install python-pip")
+    sudo("cpip install -U setuptools")
+    sudo("pip install Django")
+    sudo("systemctl start httpd")
+    sudo("yum install python-setuptools http mod_wsgi")
+
+def clean():
+    sudo("systemctl stop httpd")
+    sudo("rm -rf /var/www/html/")
+    sudo("rm -rf /etc/httpd/conf.d/finalconfig.conf")
+
+
