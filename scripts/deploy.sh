1747e29cb75454e4e12d43277ba7a28435f5df49
ironbila <octav.rusuleanu@yahoo.com>
2015-11-06 05:29:12 +0000

    solved permissions again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,6 +19,8 @@ sudo cp django-rep/utils/finalconfig.conf /etc/httpd/conf.d/ -R
 sudo usermod -a -G localuser apache
 sudo chmod 710 /home/localuser
 sudo chmod 664 /var/www/html/project-dir/backend/db.sqlite3
+sudo chown :apache /var/www/html/project-dir/backend/db.sqlite3
+sudo chown :apache /var/www/html/project-dir/backend
 
 # start server
 sudo systemctl start httpd
